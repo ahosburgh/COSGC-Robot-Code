677c5d8689be7617ce563b3ecf94ba339c7dc19a
ahosburgh <97181342+ahosburgh@users.noreply.github.com>
2022-01-15 20:32:51 -0700

    Update Bluetooth_Module_Tutorial.ino

--- Subsystems/Bluetooth_Module_Tutorial.ino
+++ Subsystems/Bluetooth_Module_Tutorial.ino
@@ -1,13 +1,23 @@
-//Make sure to download the SoftwareSerial library incase you don't have it
-//Bluetooth module RX connected to arduino TX and bluetoothe TX connected to arduino RX
-//Before uplaoding unplug RX pin, replug after uploaded succesfully
-#include <SoftwareSerial.h>
-SoftwareSerial EEBlue(0, 1); // RX | TX
+/* HC-05 Bluetooth Module Tutorial
+
+*** Wireing Instructions ***
+Arduino Pin 5v --- HC-05 VCC
+Arduino Pin GND --- HC-05 GND
+Arduino Pin TX --- HC-05 RX
+Arduino Pin RX --- HC-05 TX
+
+*** Other Notes ***
+Before uploading code, unplug the wire in the RX pin of the Arduino. Replug after uploading succesfully
+
+*/
+
+#include <SoftwareSerial.h>     
+SoftwareSerial EEBlue(0, 1);    // RX | TX
 void setup()
 {
  
   Serial.begin(9600);
-  EEBlue.begin(9600);  //Default Baud for comm, it may be different for your Module. 
+  EEBlue.begin(9600);           //Default Baud for comm, it may be different for your Module. 
   Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
  
 }
